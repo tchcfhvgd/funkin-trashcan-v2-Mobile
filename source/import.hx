12570e6dc942de2ea38a4a16f17aeb7fce3689c6
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2025-03-23 14:45:09 +0800

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -14,6 +14,30 @@ import llua.Lua;
 import backend.Achievements;
 #end
 
+// Mobile Controls
+import mobile.objects.MobileControls;
+import mobile.objects.IMobileControls;
+import mobile.objects.Hitbox;
+import mobile.objects.TouchPad;
+import mobile.objects.TouchButton;
+import mobile.input.MobileInputID;
+import mobile.backend.MobileData;
+import mobile.input.MobileInputManager;
+import mobile.backend.TouchUtil;
+
+// Android
+#if android
+import android.content.Context as AndroidContext;
+import android.widget.Toast as AndroidToast;
+import android.os.Environment as AndroidEnvironment;
+import android.Permissions as AndroidPermissions;
+import android.Settings as AndroidSettings;
+import android.Tools as AndroidTools;
+import android.os.Build.VERSION as AndroidVersion;
+import android.os.Build.VERSION_CODES as AndroidVersionCode;
+import android.os.BatteryManager as AndroidBatteryManager;
+#end
+  
 #if sys
 import sys.*;
 import sys.io.*;
